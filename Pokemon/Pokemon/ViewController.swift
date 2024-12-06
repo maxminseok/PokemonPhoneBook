ead7151a61e5d37034164d3322fa062e86129353
minseok <pms950106@gmail.com>
2024-12-06 17:41:09 +0900

    first commit

---
+++ Pokemon/Pokemon/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pokemon
+//
+//  Created by t2023-m0072 on 12/6/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
