841ff7ada6d6604dd5110fa816ad75596a3df460
minseok <pms950106@gmail.com>
2024-12-11 10:28:51 +0900

    [ADD] View 파일만 추가

---
+++ Pokemon/Pokemon/View/EditView.swift
@@ -0,0 +1,8 @@
+//
+//  EditView.swift
+//  Pokemon
+//
+//  Created by t2023-m0072 on 12/11/24.
+//
+
+import Foundation
---
+++ Pokemon/Pokemon/View/MainView.swift
@@ -0,0 +1,8 @@
+//
+//  MainView.swift
+//  Pokemon
+//
+//  Created by t2023-m0072 on 12/11/24.
+//
+
+import Foundation
